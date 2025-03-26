7535aa50490c6b04e4ce206fb60163574331c656
Balagangadhar-Dev <erootbala@gmail.com>
2025-03-26 12:47:03 +0530

    changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, Azure! This is a Node.js application.');
+  res.send('Hello, Good Afternoon! This is a Node.js application.');
 });
 
 app.listen(port, () => {
